9311cda5b2c2a623eff5bf801b719fb543f7248e
GuglielmoBini <guglielmobini@gmail.com>
2022-12-23 18:05:58 +0100

    created array, created function for posts, printed on page

--- js/script.js
+++ js/script.js
@@ -28,3 +28,122 @@ Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e inc
 */
 
 //------------------------------------------------------------
+
+// prendo l'elemeno dal DOM
+
+const postContainer = document.getElementById("container");
+
+// creo array
+
+const postsList = [
+  {
+    id: 1,
+    name: "Phil Mangione",
+    namePic: "https://unsplash.it/300/300?image=15",
+    date: "04/27/2022",
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctiominima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    postPic: "https://unsplash.it/600/300?image=171",
+    like: 80,
+  },
+  {
+    id: 2,
+    name: "Charles Leclerc",
+    namePic: "https://unsplash.it/300/300?image=17",
+    date: "03/15/2022",
+    text: "We will win the championship!",
+    postPic: "https://unsplash.it/600/300?image=173",
+    like: 3476,
+  },
+  {
+    id: 3,
+    name: "Giacomo Rossi",
+    namePic: "https://unsplash.it/300/300?image=19",
+    date: "08/13/2021",
+    text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctiominima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    postPic: "https://unsplash.it/600/300?image=181",
+    like: 65,
+  },
+  {
+    id: 4,
+    name: "Paolo Sorrentino",
+    namePic: "https://unsplash.it/300/300?image=24",
+    date: "12/25/2022",
+    text: "Buon Natale a tutti!!",
+    postPic: "https://unsplash.it/600/300?image=186",
+    like: 5466,
+  },
+  {
+    id: 5,
+    name: "Serena Williams",
+    namePic: "https://unsplash.it/300/300?image=27",
+    date: "06/12/2022",
+    text: "Roger Federer is the GOAT!",
+    postPic: "https://unsplash.it/600/300?image=165",
+    like: 126599,
+  },
+];
+
+// creo funzione per creare un post
+
+const createPost = (name, namePic, date, text, postPic, like) => {
+  const post = `
+    <div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img
+            class="profile-pic"
+            src="${namePic}"
+            alt="Phil Mangione"
+          />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${name}</div>
+          <div class="post-meta__time">${date}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">${text}</div>
+    <div class="post__image">
+      <img src="${postPic}" alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <button
+            class="like-button js-like-button"
+            href="#"
+            data-postid="1"
+          >
+            <i
+              class="like-button__icon fas fa-thumbs-up"
+              aria-hidden="true"
+            ></i>
+            <span class="like-button__label">Mi Piace</span>
+          </button>
+        </div>
+        <div class="likes__counter">
+          Piace a
+          <b id="like-counter-1" class="js-likes-counter">${like}</b> persone
+        </div>
+      </div>
+    </div>
+  </div>`;
+  return post;
+};
+
+// creo ciclo per stampare i post in pagina
+let posts = "";
+
+postsList.forEach((post) => {
+  posts += createPost(
+    post.name,
+    post.namePic,
+    post.date,
+    post.text,
+    post.postPic,
+    post.like
+  );
+});
+
+postContainer.innerHTML = posts;
